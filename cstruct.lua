9d7f268b8c160d68bee307d7fbc5818b4e79ba55
FloatingBanana <thalesmaiademoraes13@gmail.com>
2022-07-08 16:43:51 -0300

    FFI struct helper

---
+++ cstruct.lua
@@ -0,0 +1,46 @@
+-- Helper for creating C structs using FFI.
+-- If jit is disabled then it falls back to using tables, which are slower.
+
+local ffi = nil
+local hasJit = false
+
+if jit and jit.status() then
+    ffi = require "ffi"
+    hasJit = true
+end
+
+local structmt = {
+    new = NULLFUNC -- constructor
+}
+setmetatable(structmt, structmt)
+
+function structmt:__call(...)
+    local instance = nil
+
+    if hasJit then
+        instance = ffi.new(self.typename)
+    else
+        instance = setmetatable({}, self)
+    end
+
+    ---@diagnostic disable-next-line: undefined-field
+    instance:new(...)
+    return instance
+end
+
+function structmt:__index(key)
+    return rawget(structmt, key)
+end
+
+local function DefineStruct(structname, definition)
+    local struct = setmetatable({typename = structname}, structmt)
+
+    if hasJit then
+        ffi.cdef("typedef struct {"..definition.."} "..structname..";")
+        ffi.metatype(structname, struct)
+    end
+
+    return struct
+end
+
+return DefineStruct
