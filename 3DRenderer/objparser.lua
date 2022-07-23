feea88b86fef06cb86bf2f55b8fbc299e5913e3d
FloatingBanana <thalesmaiademoraes13@gmail.com>
2022-07-23 11:24:03 -0300

    Refactorate objparser

--- 3DRenderer/objparser.lua
+++ 3DRenderer/objparser.lua
@@ -1,6 +1,10 @@
+-- Default values
 local defaultuv = {u=0, v=0}
 local defaultnormal = {x=0, y=0, z=0}
 
+-----------------------------
+---------- Helpers ----------
+-----------------------------
 local function crossNormalized(v1x, v1y, v1z, v2x, v2y, v2z)
     local crossx = v1y * v2z - v2y * v1z
     local crossy = v1x * v2z - v2x * v1z
@@ -13,36 +17,66 @@ local function crossNormalized(v1x, v1y, v1z, v2x, v2y, v2z)
            crossz * invMag
 end
 
-local function push(t, elm)
-    t[#t+1] = elm
+local function tonumberMult(t)
+    for k, v in pairs(t) do
+        t[k] = tonumber(v)
+    end
+    return t
 end
 
+local function parseLine(line)
+    local f = line:gmatch("[^%s]+")
+    local param = f()
+    local args = {}
+
+    for arg in f do
+        table.insert(args, arg)
+    end
+
+    return param, args
+end
+
+
+-----------------------------
+---------- Parsers ----------
+-----------------------------
 local function ParseTextureMap(args)
     local texture = {}
 
     for i=1, #args do
         if args[i] == "-blendu" then
             texture.horizontalBlending = args[i+1] == "on"
+
         elseif args[i] == "-blendv" then
             texture.verticalBlending = args[i+1] == "on"
+
         elseif args[i] == "-boost" then
             texture.mipmapSharpnessBoost = tonumber(args[i+1])
+
         elseif args[i] == "-o" then
-            texture.offset = {u = tonumber(args[i+1]), v = tonumber(args[i+2]) or 0}
+            texture.offset = tonumberMult({u = args[i+1], v = args[i+2] or 0})
+
         elseif args[i] == "-s" then
-            texture.scale = {u = tonumber(args[i+1]), v = tonumber(args[i+2]) or 0}
+            texture.scale = tonumberMult({u = args[i+1], v = args[i+2] or 0})
+
         elseif args[i] == "-t" then
-            texture.turbulence = {u = tonumber(args[i+1]), v = tonumber(args[i+2]) or 0}
+            texture.turbulence = tonumberMult({u = args[i+1], v = args[i+2] or 0})
+
         elseif args[i] == "-texres" then
             texture.resolution = args[i+1]
+
         elseif args[i] == "-clamp" then
             texture.clamp = args[i+1] == "on"
+
         elseif args[i] == "-bm" then
             texture.bumpMultiplier = tonumber(args[i+1])
+
         elseif args[i] == "-imfchan" then
             texture.bumpChannel = args[i+1]
+
         elseif args[i] == "-type" then
             texture.reflectionMapType = args[i+1]
+
         else
             texture.texture = args[i]
         end
@@ -56,61 +90,43 @@ local function ParseMtl(filename)
     local current = nil
 
     for line in love.filesystem.lines(filename) do
-        local f = line:gmatch("[^%s]+")
-        local param = f()
-        local args = {}
-
-        for arg in f do
-            push(args, arg)
-        end
+        local param, args = parseLine(line)
 
         if param == "newmtl" then
             current = {}
             materials[args[1]] = current
-        end
 
-        if param == "Ka" then
-            current.ambientColor = {tonumber(args[1]), tonumber(args[2]), tonumber(args[3])}
-        end
+        elseif param == "Ka" then
+            current.ambientColor = tonumberMult({args[1], args[2], args[3]})
 
-        if param == "Kd" then
-            current.diffuseColor = {tonumber(args[1]), tonumber(args[2]), tonumber(args[3])}
-        end
+        elseif param == "Kd" then
+            current.diffuseColor = tonumberMult({args[1], args[2], args[3]})
 
-        if param == "Ks" then
-            current.specularColor = {tonumber(args[1]), tonumber(args[2]), tonumber(args[3])}
-        end
+        elseif param == "Ks" then
+            current.specularColor = tonumberMult({args[1], args[2], args[3]})
 
-        if param == "d" then
+        elseif param == "d" then
             current.transparence = 1 - tonumber(args[1])
-        end
 
-        if param == "Tr" then
+        elseif param == "Tr" then
             current.transparence = tonumber(args[1])
-        end
 
-        if param == "Tf" then
-            -- XYZ not supported
-            current.transmissionFilter = {tonumber(args[1]), tonumber(args[2]), tonumber(args[3])}
-        end
+        elseif param == "Tf" then
+            current.transmissionFilter = tonumberMult({args[1], args[2], args[3]}) -- XYZ not supported
 
-        if param == "Ni" then
+        elseif param == "Ni" then
             current.opticalDensity = tonumber(args[1])
-        end
 
-        if param == "illum" then
+        elseif param == "illum" then
             current.illuminationModel = tonumber(args[1])
-        end
 
-        if param == "map_Ka" then
+        elseif param == "map_Ka" then
             current.ambientTexture = ParseTextureMap(args)
-        end
 
-        if param == "map_Kd" then
+        elseif param == "map_Kd" then
             current.diffuseTexture = ParseTextureMap(args)
-        end
 
-        if param == "map_Ks" then
+        elseif param == "map_Ks" then
             current.specularTexture = ParseTextureMap(args)
         end
     end
@@ -118,7 +134,7 @@ local function ParseMtl(filename)
     return materials
 end
 
-local function Parseobj(filename, flipU, flipV, recalculateNormals, isCounterClockwise)
+local function Parseobj(filename, flipU, flipV, recalculateNormals)
     local materials = {}
     local objects = {}
 
@@ -130,13 +146,7 @@ local function Parseobj(filename, flipU, flipV, recalculateNormals, isCounterClo
     local texcoords = {}
 
     for line in love.filesystem.lines(filename) do
-        local f = line:gmatch("[^%s]+")
-        local param = f()
-        local args = {}
-
-        for arg in f do
-            push(args, arg)
-        end
+        local param, args = parseLine(line)
 
         -- Load material file
         if param == "mtllib" then
@@ -157,11 +167,11 @@ local function Parseobj(filename, flipU, flipV, recalculateNormals, isCounterClo
 
         -- Vertex
         if param == "v" then
-            push(positions, {
-                x = tonumber(args[1]),
-                y = tonumber(args[2]),
-                z = tonumber(args[3])
-            })
+            table.insert(positions, tonumberMult({
+                x = args[1],
+                y = args[2],
+                z = args[3]
+            }))
         end
 
         -- Texture coordinates
@@ -169,7 +179,7 @@ local function Parseobj(filename, flipU, flipV, recalculateNormals, isCounterClo
             local u = tonumber(args[1])
             local v = tonumber(args[2])
 
-            push(texcoords, {
+            table.insert(texcoords, {
                 u = flipU and 1 - u or u,
                 v = flipV and 1 - v or v
             })
@@ -177,11 +187,11 @@ local function Parseobj(filename, flipU, flipV, recalculateNormals, isCounterClo
 
         -- Normals
         if param == "vn" then
-            push(normals, {
-                x = tonumber(args[1]),
-                y = tonumber(args[2]),
-                z = tonumber(args[3])
-            })
+            table.insert(normals, tonumberMult({
+                x = args[1],
+                y = args[2],
+                z = args[3]
+            }))
         end
 
         -- Set material
@@ -191,7 +201,7 @@ local function Parseobj(filename, flipU, flipV, recalculateNormals, isCounterClo
                 vertices = {}
             }
 
-            push(thisobj, thisobjpart)
+            table.insert(thisobj, thisobjpart)
         end
 
         -- Faces
@@ -205,7 +215,7 @@ local function Parseobj(filename, flipU, flipV, recalculateNormals, isCounterClo
                 local tex = vt and texcoords[tonumber(vt)] or defaultuv
                 local norm = vn and normals[tonumber(vn)] or defaultnormal
 
-                push(thisobjpart.vertices, {
+                table.insert(thisobjpart.vertices, {
                     pos.x, pos.y, pos.z,
                     tex.u, tex.v,
                     norm.x, norm.y, norm.z
@@ -223,9 +233,7 @@ local function Parseobj(filename, flipU, flipV, recalculateNormals, isCounterClo
                 local vlx, vly, vlz = v3[1] - v2[1], v3[2] - v2[2], v3[3] - v2[3]
                 local normalx, normaly, normalz = crossNormalized(vfx,vfy,vfz, vlx,vly,vlz)
 
-                if isCounterClockwise then
-                    normalx, normaly, normalz = -normalx, -normaly, -normalz
-                end
+                -- normalx, normaly, normalz = -normalx, -normaly, -normalz
 
                 v1[6], v1[7], v1[8] = normalx, normaly, normalz
                 v2[6], v2[7], v2[8] = normalx, normaly, normalz
