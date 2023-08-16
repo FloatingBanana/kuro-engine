6305caa88988d5074e0d840f916c8a9f4c8c3bb7
FloatingBanana <thalesmaiademoraes13@gmail.com>
2023-08-16 10:59:58 -0300

    Added a debug utility to render cubemap faces

--- debugUtils.lua
+++ debugUtils.lua
@@ -1,3 +1,5 @@
+local Vector2 = require "engine.math.vector2"
+
 local Dbg = {}
 
 local modstates = {}
@@ -20,6 +22,34 @@ function Dbg.hotswapWhenModified(file)
     if Dbg.wasFileModified(fullFile) then
         return Dbg.hotswap(file)
     end
+end
+
+
+local cubeshader = lg.newShader [[
+uniform samplerCube cubeImg;
+uniform vec3 faceDir;
+
+vec4 effect(vec4 color, sampler2D texture, vec2 texcoords, vec2 screencoords) {
+    vec3 dir;
+
+    if (faceDir.x != 0)
+        dir = vec3(faceDir.x, texcoords.x, texcoords.y);
+    if (faceDir.y != 0)
+        dir = vec3(texcoords.x, faceDir.y, texcoords.y);
+    if (faceDir.z != 0)
+        dir = vec3(texcoords.x, texcoords.y, faceDir.z);
+
+    return Texel(cubeImg, dir) * color;
+}
+]]
+function Dbg.drawCubemapFace(cubemap, face, ...)
+    local square = Utils.newSquareMesh(Vector2(cubemap:getDimensions()))
+    
+    cubeshader:send("cubeImg", cubemap)
+    cubeshader:send("faceDir", face:toFlatTable())
+    lg.setShader(cubeshader)
+    lg.draw(square, ...)
+    lg.setShader()
 end
 
 return Dbg
