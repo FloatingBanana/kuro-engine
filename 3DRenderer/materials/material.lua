52a16e5f6cce533bb72ccdb0c1e8cbaaa69c50f0
FloatingBanana <thalesmaiademoraes13@gmail.com>
2023-08-16 09:59:28 -0300

    - Materials now only sends uniforms to it's shader when calling material:apply(). - Helper function GetTexture now returns a default blank texture if the requested texture is missing.

--- 3DRenderer/materials/material.lua
+++ 3DRenderer/materials/material.lua
@@ -1,15 +1,53 @@
+local blankTexData = love.image.newImageData(2,2)
+blankTexData:setPixel(0, 0, 0, 0, 0, 1)
+blankTexData:setPixel(1, 0, 1, 0, 1, 1)
+blankTexData:setPixel(1, 1, 0, 0, 0, 1)
+blankTexData:setPixel(0, 1, 1, 0, 1, 1)
+
+local blankTex = lg.newImage(blankTexData)
+blankTex:setFilter("nearest", "nearest")
+blankTex:setWrap("repeat")
+
+local textures = {}
+
+local function sendToShader(shader, uniform, value)
+    local sendValue = value
+
+    if not shader:hasUniform(uniform) then
+        return
+    end
+
+    if type(value) == "cdata" then
+        sendValue = value:toFlatTable()
+
+        if value.typename == "matrix" then
+            shader:send(uniform, "column", sendValue)
+            return
+        end
+    end
+
+    shader:send(uniform, sendValue)
+end
+
+
+--- @alias MaterialDefinition table<string, {uniform: string, value: any}>
+
+--- @class Material: Object
+---
+--- @field shader love.Shader
+--- @field private __attrs MaterialDefinition
+---
+--- @overload fun(shader: love.Shader, attributes: MaterialDefinition)
 local Material = Object:extend()
+
 
 function Material:new(shader, attributes)
     rawset(self, "__attrs", attributes)
     self.shader = shader
-
-    -- Small trick to properly setup the initial values
-    for key, attr in pairs(attributes) do
-        self[key] = attr.value
-    end
 end
 
+
+--- @private
 function Material:__index(key)
     if rawget(self, "__attrs") and self.__attrs[key] then
         return self.__attrs[key].value
@@ -18,6 +56,8 @@ function Material:__index(key)
     return Material[key]
 end
 
+
+--- @private
 function Material:__newindex(key, value)
     if rawget(self, "__attrs") and self.__attrs[key] then
         local attr = self.__attrs[key]
@@ -28,30 +68,29 @@ function Material:__newindex(key, value)
         end
 
         attr.value = value
-        local sendValue = value
-
-        if type(value) == "cdata" then
-            sendValue = value:toFlatTable()
-
-            local ffi = require "ffi"
-            if ffi.istype("matrix", value) then
-                self.shader:send(attr.uniform, "column", sendValue)
-                return
-            end
-        end
-
-        self.shader:send(attr.uniform, sendValue)
         return
     end
 
     rawset(self, key, value)
 end
 
+
 function Material:apply()
+    for name, attr in pairs(rawget(self, "__attrs")) do
+        if attr.value then
+            sendToShader(self.shader, attr.uniform, attr.value)
+        end
+    end
+
     lg.setShader(self.shader)
 end
 
-local textures = {}
+
+--- @param mat unknown
+--- @param type string
+--- @param texIndex integer
+--- @param linear boolean
+--- @return love.Image?
 function Material.GetTexture(mat, type, texIndex, linear)
     local path = mat:texture_path(type, texIndex)
 
@@ -62,8 +101,9 @@ function Material.GetTexture(mat, type, texIndex, linear)
 
         return textures[path]
     else
-        return nil
+        return blankTex
     end
 end
+
 
 return Material
