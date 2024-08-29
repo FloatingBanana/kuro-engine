3497ed75ad1a7c1cf0bc0d7d264c3596e8c36848
FloatingBanana <thalesmaiademoraes13@gmail.com>
2024-08-29 16:29:19 -0300

    Added direction properties to Camera3D

--- misc/camera3d.lua
+++ misc/camera3d.lua
@@ -13,6 +13,12 @@ local Object  = require "engine.3rdparty.classic.classic"
 --- @field viewMatrix Matrix
 --- @field projectionMatrix Matrix
 --- @field viewProjectionMatrix Matrix
+--- @field forward Vector3
+--- @field backward Vector3
+--- @field up Vector3
+--- @field down Vector3
+--- @field left Vector3
+--- @field right Vector3
 ---
 --- @overload fun(position: Vector3, rotation: Quaternion, fov: number, aspectRatio: number, nearPlane: number, farPlane: number): Camera3D
 local Camera = Object:extend("Camera3D")
@@ -28,7 +34,7 @@ end
 
 function Camera:__index(key)
     if key == "viewMatrix" then
-        return Matrix.CreateLookAtDirection(self.position, Vector3(0,0,1):transform(self.rotation), Vector3(0,1,0):transform(self.rotation))
+        return Matrix.CreateLookAtDirection(self.position, self.forward, self.up)
     end
 
     if key == "projectionMatrix" then
@@ -37,6 +43,30 @@ function Camera:__index(key)
 
     if key == "viewProjectionMatrix" then
         return self.viewMatrix * self.projectionMatrix
+    end
+
+    if key == "forward" then
+        return Vector3(0,0,1):transform(self.rotation)
+    end
+
+    if key == "backward" then
+        return Vector3(0,0,-1):transform(self.rotation)
+    end
+
+    if key == "up" then
+        return Vector3(0,1,0):transform(self.rotation)
+    end
+
+    if key == "down" then
+        return Vector3(0,-1,0):transform(self.rotation)
+    end
+
+    if key == "right" then
+        return Vector3(1,0,0):transform(self.rotation)
+    end
+
+    if key == "left" then
+        return Vector3(-1,0,0):transform(self.rotation)
     end
 
     return Camera[key]
