6cf66f05bbf9f075352147bb313bb2a40ab5dd9a
FloatingBanana <thalesmaiademoraes13@gmail.com>
2024-11-28 16:09:55 -0300

    Moved some IBL calculations from "cubemapUtils" to "incl_IBLCalculation" shader include file

--- misc/cubemapUtils.lua
+++ misc/cubemapUtils.lua
@@ -69,124 +69,40 @@ vec4 effect(EFFECTARGS) {
 
 
 local irradianceMapShader = ShaderEffect(vertShaderCode, [[
+#define IRRADIANCE_SAMPLE_DELTA 0.025
+
 #pragma language glsl3
+#pragma include "engine/shaders/3D/misc/incl_IBLCalculation.glsl"
 
 uniform samplerCube u_envMap;
 in vec3 v_localPos;
 
 vec4 effect(EFFECTARGS) {
-    vec3 normal = normalize(v_localPos);
-    vec3 irradiance = vec3(0.0);
-
-    vec3 right = normalize(cross(vec3(0.0,1.0,0.0), normal));
-    vec3 up = normalize(cross(normal, right));
-
-    float sampleDelta = 0.025/2.0;
-    float nrSamples = 0.0;
-    
-    for (float phi = 0.0; phi < TAU; phi += sampleDelta) {
-        for(float theta = 0.0; theta < HALF_PI; theta += sampleDelta) {
-            vec3 tangentSample = vec3(sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta));
-            vec3 sampleVec = tangentSample.x * right + tangentSample.y * up + tangentSample.z * normal;
-
-            irradiance += texture(u_envMap, sampleVec).rgb * cos(theta) * sin(theta);
-            nrSamples++;
-        }
-    }
-
-    irradiance = PI * irradiance * (1.0 / nrSamples);
-
-    return vec4(irradiance, 1.0);
+    return vec4(CalculateIrradiance(u_envMap, normalize(v_localPos)), 1.0);
 }
 ]])
 
 
 local environmentRadianceShader = ShaderEffect(vertShaderCode, [[
+#define ENVIRONMENT_RADIANCE_SAMPLE_COUNT 32
+
 #pragma language glsl3
 #pragma include "engine/shaders/3D/misc/incl_IBLCalculation.glsl"
-#pragma include "engine/shaders/3D/misc/incl_PBRLighting.glsl"
 
 in vec3 v_localPos;
 uniform samplerCube u_envMap;
 uniform float u_roughness;
 
-const uint SAMPLE_COUNT = 4096u;
-
 vec4 effect(EFFECTARGS) {
-    vec3 N = normalize(v_localPos);
-    vec3 R = N;
-    vec3 V = N;
-
-    vec3 prefilteredColor = vec3(0.0);
-    float totalWeight = 0.0;
-    
-    for (uint i = 0u; i < SAMPLE_COUNT; ++i) {
-        vec2 Xi = Hammersley(i, SAMPLE_COUNT);
-        vec3 H = ImportanceSampleGGX(Xi, N, u_roughness);
-        vec3 L = normalize(2.0 * dot(V, H) * H - V);
-
-        float NdotL = max(dot(N, L), 0.0);
-        if (NdotL > 0.0) {
-            float HdotV = max(dot(H, V), 0.0);
-            float NdotH = max(dot(N, H), 0.0);
-            float D = DistributionGGX(NdotH, u_roughness);
-            float pdf = (D * NdotH / (4.0 * HdotV)) + 0.0001;
-
-            float resolution = textureSize(u_envMap, 0).x;
-            float saTexel = 4.0 * PI / (6.0 * resolution * resolution);
-            float saSample = 1.0 / (float(SAMPLE_COUNT) * pdf + 0.0001);
-            float mipLevel = (u_roughness == 0.0) ? 0.0 : 0.5 * log2(saSample / saTexel);
-
-            prefilteredColor += textureLod(u_envMap, L, mipLevel).rgb * NdotL;
-            totalWeight += NdotL;
-        }
-    }
-    
-    return vec4(prefilteredColor / totalWeight, 1.0);
+    return vec4(CalculateEnvironmentRadiance(u_envMap, normalize(v_localPos), u_roughness), 1.0);
 }
 ]])
 
 local calculateBRDF_LUTShader = ShaderEffect [[
+#define BRDF_SAMPLE_COUNT 1024
+
 #pragma language glsl3
 #pragma include "engine/shaders/3D/misc/incl_IBLCalculation.glsl"
-
-const uint SAMPLE_COUNT = 1024u;
-
-vec2 integrateBRDF(float NdotV, float roughness) {
-    vec3 V;
-    V.x = sqrt(1.0 - NdotV*NdotV);
-    V.y = 0.0;
-    V.z = NdotV;
-
-    float A = 0.0;
-    float B = 0.0;
-
-    vec3 N = vec3(0.0, 0.0, 1.0);
-
-    for (uint i = 0u; i < SAMPLE_COUNT; ++i) {
-        vec2 Xi = Hammersley(i, SAMPLE_COUNT);
-        vec3 H = ImportanceSampleGGX(Xi, N, roughness);
-        vec3 L = normalize(2.0 * dot(V, H) * H - V);
-
-        float NdotL = max(L.z, 0.0);
-        float NdotH = max(H.z, 0.0);
-        float VdotH = max(dot(V, H), 0.0);
-
-        if (NdotL > 0.0) {
-            float G = GeometrySmith_IBL(dot(N, V), dot(N, L), roughness);
-            float G_Vis = (G * VdotH) / (NdotH * NdotV);
-            float Fc = pow(1.0 - VdotH, 5.0);
-
-            A += (1.0 - Fc) * G_Vis;
-            B += Fc * G_Vis;
-        }
-    }
-
-    A /= float(SAMPLE_COUNT);
-    B /= float(SAMPLE_COUNT);
-
-    return vec2(A, B);
-}
 
 vec4 effect(vec4 color, sampler2D tex, vec2 texcoords, vec2 screencoords) {
     vec2 brdf = integrateBRDF(texcoords.x, 1.0 - texcoords.y);
--- shaders/3D/misc/incl_IBLCalculation.glsl
+++ shaders/3D/misc/incl_IBLCalculation.glsl
@@ -1,5 +1,7 @@
 #pragma language glsl3
 
+#pragma include "engine/shaders/3D/misc/incl_PBRLighting.glsl"
+
 vec2 Hammersley(uint i, uint N) {
     // Van Der Corput sequence
     uint bits = i;
@@ -40,7 +42,7 @@ vec3 ImportanceSampleGGX(vec2 Xi, vec3 N, float roughness) {
 
 float GeometrySchlickGGX_IBL(float NdotV, float roughness) {
     float r = roughness;
-    float k = (r*r) * 0.5;// / 2.0;
+    float k = (r*r) * 0.5;
     float denom = NdotV * (1.0 - k) + k;
     
     return NdotV / denom;
@@ -52,4 +54,107 @@ float GeometrySmith_IBL(float NdotV, float NdotL, float roughness) {
     float ggx1 = GeometrySchlickGGX_IBL(NdotL, roughness);
 
     return ggx1 * ggx2;
+}
+
+
+
+#ifndef BRDF_SAMPLE_COUNT
+#   define BRDF_SAMPLE_COUNT 1024
+#endif
+
+vec2 integrateBRDF(float NdotV, float roughness) {
+    vec3 V;
+    V.x = sqrt(1.0 - NdotV*NdotV);
+    V.y = 0.0;
+    V.z = NdotV;
+
+    vec3 N = vec3(0.0, 0.0, 1.0);
+    float A = 0.0;
+    float B = 0.0;
+
+    for (uint i = 0u; i < uint(BRDF_SAMPLE_COUNT); ++i) {
+        vec2 Xi = Hammersley(i, uint(BRDF_SAMPLE_COUNT));
+        vec3 H = ImportanceSampleGGX(Xi, N, roughness);
+        vec3 L = normalize(2.0 * dot(V, H) * H - V);
+
+        float NdotL = max(L.z, 0.0);
+        float NdotH = max(H.z, 0.0);
+        float VdotH = max(dot(V, H), 0.0);
+
+        if (NdotL > 0.0) {
+            float G = GeometrySmith_IBL(dot(N, V), dot(N, L), roughness);
+            float G_Vis = (G * VdotH) / (NdotH * NdotV);
+            float Fc = pow(1.0 - VdotH, 5.0);
+
+            A += (1.0 - Fc) * G_Vis;
+            B += Fc * G_Vis;
+        }
+    }
+
+    return vec2(A, B) / BRDF_SAMPLE_COUNT;
+}
+
+
+
+#ifndef IRRADIANCE_SAMPLE_DELTA
+#   define IRRADIANCE_SAMPLE_DELTA 0.025
+#endif
+
+vec3 CalculateIrradiance(samplerCube environment, vec3 normal) {
+    vec3 right = normalize(cross(vec3(0.0,1.0,0.0), normal));
+    vec3 up = normalize(cross(normal, right));
+
+    vec3 irradiance = vec3(0.0);
+    float sampleCount = 0.0;
+    
+    for (float phi = 0.0; phi < TAU; phi += IRRADIANCE_SAMPLE_DELTA) {
+        for(float theta = 0.0; theta < HALF_PI; theta += IRRADIANCE_SAMPLE_DELTA) {
+            vec3 tangentSample = vec3(sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta));
+            vec3 sampleVec = tangentSample.x * right + tangentSample.y * up + tangentSample.z * normal;
+
+            irradiance += texture(environment, sampleVec).rgb * cos(theta) * sin(theta);
+            sampleCount++;
+        }
+    }
+
+    return PI * irradiance / sampleCount;
+}
+
+
+
+#ifndef ENVIRONMENT_RADIANCE_SAMPLE_COUNT
+#   define ENVIRONMENT_RADIANCE_SAMPLE_COUNT 1024
+#endif
+
+vec3 CalculateEnvironmentRadiance(samplerCube environment, vec3 N, float roughness) {
+    vec3 R = N;
+    vec3 V = N;
+
+    float resolution = textureSize(environment, 0).x;
+    float saTexel = 4.0 * PI / (6.0 * resolution * resolution);
+
+    vec3 prefilteredColor = vec3(0.0);
+    float totalWeight = 0.0;
+
+    for (uint i = 0u; i < uint(ENVIRONMENT_RADIANCE_SAMPLE_COUNT); ++i) {
+        vec2 Xi = Hammersley(i, uint(ENVIRONMENT_RADIANCE_SAMPLE_COUNT));
+        vec3 H = ImportanceSampleGGX(Xi, N, roughness);
+        vec3 L = normalize(2.0 * dot(V, H) * H - V);
+
+        float NdotL = max(dot(N, L), 0.0);
+        if (NdotL > 0.0) {
+            float HdotV = max(dot(H, V), 0.0);
+            float NdotH = max(dot(N, H), 0.0);
+            float D = DistributionGGX(NdotH, roughness);
+            float pdf = (D * NdotH / (4.0 * HdotV)) + 0.0001;
+
+            float saSample = 1.0 / (float(ENVIRONMENT_RADIANCE_SAMPLE_COUNT) * pdf);
+            float mipLevel = (roughness == 0.0) ? 0.0 : 0.5 * log2(saSample / saTexel);
+
+            prefilteredColor += textureLod(environment, L, mipLevel).rgb * NdotL;
+            totalWeight += NdotL;
+        }
+    }
+
+    return prefilteredColor / totalWeight;
 }
