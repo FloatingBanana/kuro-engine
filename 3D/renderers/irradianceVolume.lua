5221c461bc55fe6a6205f2688ad857c030e4c8fc
FloatingBanana <thalesmaiademoraes13@gmail.com>
2025-03-31 00:24:28 -0300

    Fixed IrradianceVolume:bakeFromSolidColor()

--- 3D/renderers/irradianceVolume.lua
+++ 3D/renderers/irradianceVolume.lua
@@ -96,7 +96,13 @@ end
 ---@param color Vector3
 function IrradianceVolume:bakeFromSolidColor(color)
     self:mapProbes(function(probe, index)
-        return SH9Color(color, color, color, color, color, color, color, color, color)
+        return
+            (SH9Color.ProjectDirection(Vector3(1,0,0):multiply(color)) +
+            SH9Color.ProjectDirection(Vector3(0,1,0):multiply(color)) +
+            SH9Color.ProjectDirection(Vector3(0,0,1):multiply(color)) +
+            SH9Color.ProjectDirection(Vector3(-1,0,0):multiply(color)) +
+            SH9Color.ProjectDirection(Vector3(0,-1,0):multiply(color)) +
+            SH9Color.ProjectDirection(Vector3(0,0,-1):multiply(color))):multiply(1/6)
     end)
 end
 
