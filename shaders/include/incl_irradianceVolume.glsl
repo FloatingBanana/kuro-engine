12d089de3b4c79f6db3f1bd6d8d170206c9d2435
FloatingBanana <thalesmaiademoraes13@gmail.com>
2025-05-20 00:59:56 -0300

    Optimized Irradiance volume trilinear interpolation and simplified the include file

--- shaders/include/incl_irradianceVolume.glsl
+++ shaders/include/incl_irradianceVolume.glsl
@@ -3,7 +3,6 @@
 
 struct IrradianceVolume {
     sampler2D probeBuffer;
-    mat4 transform;
     mat4 invTransform;
     vec3 gridSize;
 };
@@ -39,66 +38,35 @@ SH9Color IrrV_getProbe(IrradianceVolume volume, int index) {
     return probe;
 }
 
-SH9Color IrrV_getProbe(IrradianceVolume volume, ivec3 cell) {
-    return IrrV_getProbe(volume, IrrV_getIndex(volume, cell));
-}
 
-vec3 IrrV_getCellPosition(IrradianceVolume volume, ivec3 cell) {
-    vec3 cellCenter = 1.0 / volume.gridSize * 0.5;
-    vec3 probePos = (cell / volume.gridSize) + cellCenter - 0.5;
-
-    return (volume.transform * vec4(probePos, 1.0)).xyz;
-}
-
-
-ivec3 IrrV_getNearestCell(IrradianceVolume volume, vec3 pos) {
-    vec3 localPos = (volume.invTransform * vec4(pos, 1.0)).xyz;
-    localPos = (localPos + 0.5) * volume.gridSize;
-
-    return ivec3(localPos);
-}
-
-void IrrV_getNeighborCells(IrradianceVolume volume, vec3 pos, out ivec3 neighbors[8], out vec3 distances) {
-    vec3 localPos = (volume.invTransform * vec4(pos, 1.0)).xyz;
-    localPos = (localPos + 0.5) * volume.gridSize;
-
-    vec3 signedDist = fract(localPos) - 0.5;
-    ivec3 n = ivec3(sign(signedDist));
-    ivec3 cell = ivec3(localPos);
-
-    distances = abs(signedDist);
-    neighbors = ivec3[] (
-        cell,
-        cell + n * ivec3(1,0,0),
-        cell + n * ivec3(0,1,0),
-        cell + n * ivec3(1,1,0),
-        cell + n * ivec3(0,0,1),
-        cell + n * ivec3(1,0,1),
-        cell + n * ivec3(0,1,1),
-        cell + n * ivec3(1,1,1)
-    );
-}
+const ivec3 neighborOffsets[8] = ivec3[] (
+    ivec3(0,0,0),
+    ivec3(1,0,0),
+    ivec3(0,1,0),
+    ivec3(1,1,0),
+    ivec3(0,0,1),
+    ivec3(1,0,1),
+    ivec3(0,1,1),
+    ivec3(1,1,1)
+);
 
 vec3 IrrV_getIrradiance(IrradianceVolume volume, vec3 pos, vec3 normal) {
-    ivec3 neighbors[8];
-    vec3 distances;
-    IrrV_getNeighborCells(volume, pos, neighbors, distances);
+    vec3 localPos = (volume.invTransform * vec4(pos, 1.0)).xyz;
+    localPos = (localPos + 0.5) * volume.gridSize;
 
-    vec3 probeIrr[8];
+    ivec3 n = ivec3(sign(fract(localPos) - 0.5));
+
+    vec3 res;
     for (int i=0; i < 8; i++) {
-        ivec3 cell = clamp(neighbors[i], ivec3(0,0,0), ivec3(volume.gridSize)-1);
-        SH9Color probe = IrrV_getProbe(volume, cell);
+        ivec3 cell = ivec3(localPos) + n * neighborOffsets[i];
+        int index = IrrV_getIndex(volume, clamp(cell, ivec3(0,0,0), ivec3(volume.gridSize)-1));
 
-        probeIrr[i] = EvaluateSH(probe, normal.xzy);
+        vec3 probeDir = cell - (localPos - 0.5);
+        vec3 trilinear = vec3(1,1,1) - clamp(abs(probeDir), vec3(0,0,0), vec3(1,1,1));
+
+        SH9Color probe = IrrV_getProbe(volume, index);
+        res += trilinear.x * trilinear.y * trilinear.z * EvaluateSH(probe, normal);
     }
 
-    vec3 nearBottom = mix(probeIrr[0], probeIrr[1], distances.x);
-    vec3 nearTop = mix(probeIrr[2], probeIrr[3], distances.x);
-    vec3 near = mix(nearBottom, nearTop, distances.y);
-
-    vec3 farBottom = mix(probeIrr[4], probeIrr[5], distances.x);
-    vec3 farTop = mix(probeIrr[6], probeIrr[7], distances.x);
-    vec3 far = mix(farBottom, farTop, distances.y);
-
-    return mix(near, far, distances.z);
+    return res;
 }
