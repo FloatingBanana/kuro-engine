a059e55d5a8788be67fdb68b3d7b0b4b7c107c56
FloatingBanana <thalesmaiademoraes13@gmail.com>
2025-08-28 19:28:59 -0300

    Deque and Queue internal behavior were rewritten to a much more simpler alternative

--- collections/deque.lua
+++ collections/deque.lua
@@ -9,9 +9,6 @@ local Object = require "engine.3rdparty.classic.classic"
 local Deque = Object:extend("Deque")
 
 function Deque:new(...)
-    self.first = 0
-    self.last = -1
-
     for i=1, select("#", ...)do
         self:pushRight(select(i, ...))
     end
@@ -21,90 +18,45 @@ end
 --- Pushes an item to the left side of the deque
 --- @param item any: Item to be pushed
 function Deque:pushLeft(item)
-    self.first = self.first - 1
-    self[self.first] = item
+    table.insert(self, 1, item)
 end
 
 
 --- Pushes an item to the right side of the deque
 --- @param item any: Item to be pushed
 function Deque:pushRight(item)
-    self.last = self.last + 1
-    self[self.last] = item
+    table.insert(self, item)
 end
 
 
 --- Removes the leftmost item and returns it
 --- @return any: The popped item
 function Deque:popLeft()
-    assert(self:getLength() > 0, "Deque is empty")
-
-    local item = self:peekLeft()
-    self[self.first] = nil
-    self.first = self.first + 1
-
-    return item
+    assert(self[1], "Deque is empty")
+    return table.remove(self, 1)
 end
 
 
 --- Removes the rightmost item and returns it
 --- @return any: The popped item
 function Deque:popRight()
-    assert(self:getLength() > 0, "Deque is empty")
-
-    local item = self:peekRight()
-    self[self.last] = nil
-    self.last = self.last - 1
-
-    return item
+    assert(self[1], "Deque is empty")
+    return table.remove(self)
 end
 
 
 --- Returns the leftmost item without removing it
 --- @return any: The leftmost item
 function Deque:peekLeft()
-    return self[self.first]
+    return self[1]
 end
 
 
 --- Returns the rightmost item without removing it
 --- @return any: The rightmost item
 function Deque:peekRight()
-    return self[self.last]
+    return self[#self]
 end
 
-
---- Get the number of items in this deque
---- @return number: The number of items
-function Deque:getLength()
-    return math.abs(self.last - self.first + 1)
-end
-
-
---- Gets item at the specified index
---- @param index number: Index of item
---- @return any: The item at the specified index
-function Deque:getItem(index)
-    return self[self.first + (index - 1)]
-end
-
-
-local function iter(this, i)
-    i = i+1
-    local item = this:getItem(i)
-
-    if item then
-        return item, i
-    end
-end
-
-
---- Loops through all items in this deque. Use this instead of `ipairs`
---- @return function: Iterator
---- @return Deque: This deque
---- @return number: First index
-function Deque:iterate()
-    return iter, self, 0
-end
 
 return Deque
--- collections/queue.lua
+++ collections/queue.lua
@@ -1,4 +1,3 @@
-local Deque = require "engine.collections.deque"
 local Object = require "engine.3rdparty.classic.classic"
 
 
@@ -11,9 +10,6 @@ local Object = require "engine.3rdparty.classic.classic"
 local Queue = Object:extend("Queue")
 
 function Queue:new(...)
-    self.first = 0
-    self.last = -1
-
     for i=1, select("#", ...)do
         self:push(select(i, ...))
     end
@@ -23,52 +19,22 @@ end
 --- Pushes an item to the top of the queue
 --- @param item any: Item to be pushed
 function Queue:push(item)
-    self.last = self.last + 1
-    self[self.last] = item
+    table.insert(self, item)
 end
 
 
 --- Removes the bottommost item and returns it
 --- @return any: The popped item
 function Queue:pop()
-    assert(self:getLength() > 0, "Queue is empty")
-
-    local item = self:peek()
-    self[self.first] = nil
-    self.first = self.first + 1
-
-    return item
+    assert(self[1], "Queue is empty")
+    return table.remove(self, 1)
 end
 
 
 --- Returns the bottommost item without removing it
 --- @return any: The bottommost item
 function Queue:peek()
-    return self[self.first]
-end
-
--- Reusing some functions from Deque since they would have the same logic
-
---- Get the number of items in this queue
---- @return number: The number of items
-function Queue:getLength()
-    return math.abs(self.last - self.first + 1)
-end
-
---- Gets item at the specified index
---- @param index number: Index of item
---- @return any: The item at the specified index
-function Queue:getItem(index)
-    return self[self.first + (index - 1)]
-end
-
-
---- Loops through all items in this Queue. Use this instead of `ipairs`
---- @return function: Iterator
---- @return Queue: This queue
---- @return number: First index
-function Queue:iterate()
-    return Deque.iterate(self) --- @diagnostic disable-line
+    return self[1]
 end
 
 
